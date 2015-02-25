0af46a4b11228ea4df9ba49cafe9d1823e55275a
Shikhar <apple@Apples-MacBook-Pro.local>
2015-02-25 18:08:41 +0530

    changed middleEnd to have a private Client pointer

--- Code/middleEnd.cpp
+++ Code/middleEnd.cpp
@@ -1,64 +1,65 @@
 #include "middleEnd.h"
 
-bool middleEnd::Register(Client& c, string& username, string& password)
+bool middleEnd::Register(string& username, string& password)
 {
-	c.writeCommand("register");
-	c.writeCommand(username);
-	c.writeCommand(password);
-	return c.verifyUser("Successful");
+	c->writeCommand("register");
+	c->writeCommand(username);
+	c->writeCommand(password);
+	return c->verifyUser("Successful");
 
 }
 
 
 
-bool middleEnd::login(Client& c, string& username, string& password)
+bool middleEnd::login(string& username, string& password)
 {
-	c.writeCommand("login");
-	c.writeCommand(username);
+	c->writeCommand("login");
+	c->writeCommand(username);
 
-	c.writeCommand(password);
-	return c.verifyUser("Valid");
+	c->writeCommand(password);
+	return c->verifyUser("Valid");
 }
 
 
-void middleEnd::download(string& username, Client& c, string& fileName)
+void middleEnd::download(string& username, string& fileName)
 {
-	c.writeCommand("download");
+	c->writeCommand("download");
 	
-	if (!c.checkIfAllowed(username, fileName))
+	if (!c->checkIfAllowed(username, fileName))
 	{
 		printf("%s\n", "Permission Denied");
 		exit(1);
 	}
 
-	c.readFromServer();
+	c->readFromServer();
 
 	// make changes to u?
 }
 
 
-void middleEnd::upload(Client& c, string& fileName)
+void middleEnd::upload(string& username ,string& fileName)
 {
-	c.writeCommand("upload");
-	c.writeToServer(fileName);
+	c->writeCommand("upload");
+	c->writeCommand(username);
+	c->writeToServer(fileName);
 	// make changes to u?
 }
 
 
-void middleEnd::share(Client& c, string& fileName, string& uA, string& uB)
+void middleEnd::share(string& fileName, string& uA, string& uB)
 {
 	
-	c.writeCommand("share");
-	c.writeCommand(uA);
-	c.writeCommand(uB);
-	c.writeCommand(fileName);
+	c->writeCommand("share");
+	c->writeCommand(uA);
+	c->writeCommand(uB);
+	c->writeCommand(fileName);
 	/* share file between user a and user b */
 }
 
-void middleEnd::Delete(Client& c, string& fileName)
+void middleEnd::Delete(string& username, string& fileName)
 {
-	c.writeCommand("delete");
-	c.writeCommand(fileName);
-
+	c->writeCommand("delete");
+	c->writeCommand(username);
+	c->writeCommand(fileName);
 	/* delete file from client c */
 }
